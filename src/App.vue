6521af9b2fc30e611dc0a9d8b71d9ddf6ff1ad0c
makhmudov-otabek <mahmudovotabek2208@gmail.com>
2023-09-19 14:40:38 +0500

    first commit and making custom classes

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <!-- <RouterLink :to="{ name: 'home' }">Home</RouterLink> -->
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <Login />
+  </div>
+</template>
+
+<script>
+import Login from "../components/Login.vue";
+
+export default {
+  components: { Login },
+};
+</script>
+
+<style scoped></style>
